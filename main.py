29b869ed3d1505381cea24ecd1b857d9c58441f2
dimkauzh <uzhdimka@gmail.com>
2022-06-07 21:07:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Modules
 import pygame
 from sys import exit
 
@@ -14,13 +15,17 @@ clock = pygame.time.Clock()
 # Font
 font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
+# Snail position
+snail_x_pos = 600
+snail_y_pos = 265
+
 # For stopping game(Moving)Surfaces
 def kill():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+            
 # Surfaces + Moving Surfaces
 def blits():
     screen.blit(sky_surface, (0, 0))
@@ -36,13 +41,20 @@ text_surface = font.render("My game", False, "Black")
 # Player + Bots (Moving Surfaces)
 snail_surface = pygame.image.load("graphics/snail/snail1.png")
 
-# Snail position
-snail_x_pos = 600
-snail_y_pos = 265
-
-# Loop while game is running
+# While loop while the game is running
 while True:
+    
+    # Functions from before
     kill()
     blits()
+    
+    # Moving the snail
+    snail_x_pos -= 3
+    
+    # Changes the direction that the snail goes
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    
+    # Update display
     pygame.display.update()
     clock.tick(60)
